54d9b37e4b930e84e53b3ed6c60fa47f608b223e
Lucian-CTLA <LucianCantarella@outlook.com>
2026-08-07 22:15:02 +0800

    指针案例

---
+++ sixth/指针案例/指针案例/指针案例.cpp
@@ -0,0 +1,34 @@
+//指针案例
+//封装一个函数，利用冒泡排序，实现对整型数组的升序排序 int arr[10]={4,3,6,9,1,2,10,8,7,5}
+#include<iostream>
+using namespace std;
+//冒泡排序 参数1 数组的首地址 参数2数组长度
+//🍕
+void bubbleSort(int *arr,int len) {
+	for(int i=0;i<len-1;i++ )
+		for (int j = 0;j < len - i-1;j++) {
+			//如果j>j+i的值那么就交换两数
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+}
+//打印数组
+void printArray(int* arr, int len) {
+	for (int i = 0;i < len;i++) {
+		cout << arr[i] << endl;
+	}
+}
+int main() {
+	//1.创建一个数组
+	int arr[10] = { 4,3,6,9,1,2,10,8,7,5 };
+	//数组长度
+	int len = sizeof(arr)/sizeof(arr[0]);
+	//2.创建一个函数实现冒泡排序
+	bubbleSort(arr,len);
+	//3.打印排序后的数组
+	printArray(arr, len);//打印数组的函数
+	return 0;
+}
