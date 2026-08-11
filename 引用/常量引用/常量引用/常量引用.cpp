4eb051ae2dfccefe66b27a63f4cf457deba809f0
Lucian-CTLA <LucianCantarella@outlook.com>
2026-08-12 00:05:20 +0800

    常量引用

---
+++ 引用/常量引用/常量引用/常量引用.cpp
@@ -0,0 +1,21 @@
+//常量引用主要来修饰形参，防止误操作
+//可以加const修饰形参，防止形参改变实参
+#include<iostream>
+using namespace std;
+//打印数据函数
+void showValue(int& val) {
+	val = 10000;
+	cout <<val <<endl;
+
+}
+
+int main() {
+	//int a = 10;
+	//加上const后 编译器将代码创建为 int temp=10;const int &ref=temp;
+	//const int& fer = 10;//引用必须一块合法存储空间
+	//ref=20;//加入const变为只读，不可以修改
+	int a = 100;
+	showValue(a);
+	cout << a << endl;
+	return 0;
+}
