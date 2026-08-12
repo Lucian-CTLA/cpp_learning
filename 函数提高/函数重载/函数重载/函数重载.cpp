a2e46b779e0c689201faccde0e68b46b8342ff67
Lucian-CTLA <LucianCantarella@outlook.com>
2026-08-12 23:52:52 +0800

    更新函数的重载

--- 函数提高/函数重载/函数重载/函数重载.cpp
+++ 函数提高/函数重载/函数重载/函数重载.cpp
@@ -1,13 +1,35 @@
-//函数名可以相同，提高复用性
 /*
+函数重载
 满足条件：
 同一个作用域下
 函数名称相同
 函数参数类型不同或者个数不同或者顺序不同
 */
+
+
 #include<iostream>
 using namespace std;
+
+void func() {
+    cout << "func" << endl;
+}
+
+void func(int a) {
+    cout << "func (int a)" << endl;
+}
+void func(double a) {
+    cout << "func(double)" << endl;
+}
+void func(int a, double b) {
+    cout << "func(int a double b)" << endl;
+}
+
+void func(double a, int b) {
+    cout << "func(double a int b)" << endl;
+}
+// 函数的返回值不可以作为函数的重载的条件
 int main() {
-	
-	return 0;
+    func(20,3.14);
+    func(20.2, 3);// 调用有参版本（第二个函数），输出 "func (int a)"
+    return 0;
 }
