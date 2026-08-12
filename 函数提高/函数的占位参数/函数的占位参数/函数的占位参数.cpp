363caff3a6c2cfc28e47b04f12e88f0c9cd6f026
Lucian-CTLA <LucianCantarella@outlook.com>
2026-08-12 22:37:17 +0800

    函数的占位参数和函数的重载

---
+++ 函数提高/函数的占位参数/函数的占位参数/函数的占位参数.cpp
@@ -0,0 +1,12 @@
+//c++中函数的形参列表里可以有占位参数，用来做占位，调用函数时必须填补该位置
+//语法：返回值类型 函数名 （数据类型）{}
+#include<iostream>
+using namespace std;
+//展位参数可以有默认参数
+void func(int a,int=10) {//int占位
+	cout << "func" << endl;
+}
+int main() {
+	func(10);
+	return 0;
+}
