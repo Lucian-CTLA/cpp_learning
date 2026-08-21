62c35961e0c06dd139a454faa61c5c1f692a5180
Lucian-CTLA <LucianCantarella@outlook.com>
2026-08-21 22:21:31 +0800

    更新封装案例2

--- 类和对象/封装/封装案例2/封装案例2/封装案例2.cpp
+++ 类和对象/封装/封装案例2/封装案例2/封装案例2.cpp
@@ -4,24 +4,43 @@
 #include<string>
 using namespace std;
 
-class student {
-public:
+//class student {
+//public:
+//	string name;
+//	string num;
+//	string s1(){
+//		return "name: " + name + " number: " + num;
+//	}
+//
+//};
+class studnet {
+public://公共权限
+	//属性：
 	string name;
-	string num;
+	int id;
 	string s1(){
-		return "name: " + name + " number: " + num;
+		return  name;
+	}
+	int s2() {
+		return id;
 	}
 
 };
 
 int main() {
-	student sn;
+	/*student sn;
 	string sa, su;
 	cin >> sa >> su;
 	sn.name=sa;
 	sn.num=su;
 	
-	cout << sn.s1() << endl;
-
+	cout << sn.s1() << endl;*/
+	studnet sn;
+	string sn_name;
+	int sn_id;
+	cin >> sn_name >> sn_id;
+	sn.name = sn_name;
+	sn.id = sn_id;
+	cout << "name: " << sn.s1() << " " << "id: " << sn.s2() << endl;
 	return 0;
 }
